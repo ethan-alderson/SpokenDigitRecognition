6106eec797940b35df95e472d04f3747d14a7f5a
Ethan <ethanalderson777@gmail.com>
2022-12-30 21:13:33 -0500

    Wrote initial audiodataset class

--- testCode.py
+++ testCode.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 # opens audio files in python format, used here to convert audio into spectrograms
 import librosa
 
+# grab my data set class
+import SpokenDigitRecognition.audiodataset as audiodataset
 
 audio = librosa.load("SpokenDigitRecognition/recordings/0_george_0.wav", sr=8000)
 waveform = audio[0]
@@ -20,4 +22,9 @@ waveform = audio[0]
 melspec = librosa.feature.melspectrogram(y = waveform, sr=8000)
 
 plt.imshow(librosa.power_to_db(melspec, ref=np.max))
+
+dataset = audiodataset()
+
+
+plt.imshow(librosa.power_to_db(dataset.__getitem(10)[0], ref=np.max))
 plt.show()
