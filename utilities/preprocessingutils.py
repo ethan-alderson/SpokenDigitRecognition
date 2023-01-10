bd0683f97d82d6ef2bde9c07b41a58748c071404
Ethan <ethanalderson777@gmail.com>
2023-01-09 22:33:44 -0500

    Organizing file structure

--- utilities/preprocessingutils.py
+++ utilities/preprocessingutils.py
@@ -37,3 +37,11 @@ class PreprocessingUtils:
 
         audio = librosa.load(f'SpokenDigitRecognition/recordings/{filename}', sr=8000)
         return self.trim_and_pad(librosa.feature.melspectrogram(y = audio[0], sr=8000, n_fft=16384))
+
+    def parse_data(self):
+        # 80% for training and 20% for testing
+        files = pd.read_csv("SpokenDigitRecognition/train.csv")
+        train = files.iloc[:int(3000*0.8)]
+        test  = files.iloc[int(3000*0.8):]
+        train.to_csv('SpokenDigitRecognition/train_data.csv')
+        test.to_csv('SpokenDigitRecognition/test_data.csv')
